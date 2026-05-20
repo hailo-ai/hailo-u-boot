301765d7cbc2c7787666fbf58c040276e59ceab7
HailoVPU-Automation <hailovpu-automation@hailo.ai>
2026-05-20 12:06:10 +0300

    v1.11.0-dv-LGL_35

--- include/configs/hailo15l_common.h
+++ include/configs/hailo15l_common.h
@@ -8,7 +8,7 @@
 #ifndef __HAILO15L_COMMON_H
 #define __HAILO15L_COMMON_H
 
-#ifndef CONFIG_HAILO15_EMMC_8BIT
+#if !defined(CONFIG_HAILO15_EMMC_8BIT) && !defined(CONFIG_HAILO_NAND_BOOT)
 #define BOOTMENU \
     /* Try all boot options by order */ \
     "bootmenu_0=Autodetect=" \
@@ -22,7 +22,7 @@
     "bootmenu_3=Boot from NFS=run bootnfs\0" \
     "default_spl_boot_source=" SPL_BOOT_SOURCE "\0" \
     "spl_boot_source=" SPL_BOOT_SOURCE "\0"
-#else
+#elif defined(CONFIG_HAILO15_EMMC_8BIT) && !defined(CONFIG_HAILO_NAND_BOOT)
 #define BOOTMENU \
     /* Try all boot options by order */ \
     "bootmenu_0=Autodetect=" \
@@ -35,9 +35,38 @@
     "bootmenu_2=Boot from NFS=run bootnfs\0" \
     "default_spl_boot_source=mmc2\0" \
     "spl_boot_source=mmc2\0"
-#endif
-
-#define BOOT_COMMAND "bootm ${fitimage_ram_addr}"
+#elif !defined(CONFIG_HAILO15_EMMC_8BIT) && defined(CONFIG_HAILO_NAND_BOOT)
+#define BOOTMENU \
+    /* Try all boot options by order */ \
+    "bootmenu_0=Autodetect=" \
+        "if test ${boot_image_mode} = 1; then run boot_swupdate_mmc; exit 1; fi; " \
+        "if test \"${auto_uboot_update_enable}\" = \"yes\"; then run auto_uboot_update; exit 1; fi; " \
+        "echo Trying Boot from NAND Flash; run boot_nand_flash;" \
+        "echo Trying Boot from eMMC; run boot_mmc1;" \
+        "echo Trying Boot from NFS; run bootnfs;" \
+        "echo ERROR: All boot options failed\0" \
+    "bootmenu_1=Boot from NAND Flash=run boot_nand_flash\0" \
+    "bootmenu_2=Boot from SD Card=run boot_mmc0\0" \
+    "bootmenu_3=Boot from eMMC=run boot_mmc1\0" \
+    "bootmenu_4=Boot from NFS=run bootnfs\0" \
+    "default_spl_boot_source=" SPL_BOOT_SOURCE "\0" \
+    "spl_boot_source=" SPL_BOOT_SOURCE "\0"
+#else /* CONFIG_HAILO15_EMMC_8BIT && CONFIG_HAILO_NAND_BOOT */
+#define BOOTMENU \
+    /* Try all boot options by order */ \
+    "bootmenu_0=Autodetect=" \
+        "if test ${boot_image_mode} = 1; then run boot_swupdate_mmc; exit 1; fi; " \
+        "if test \"${auto_uboot_update_enable}\" = \"yes\"; then run auto_uboot_update; exit 1; fi; " \
+        "echo Trying Boot from NAND Flash; run boot_nand_flash;" \
+        "echo Trying Boot from eMMC; run boot_mmc1;" \
+        "echo Trying Boot from NFS; run bootnfs;" \
+        "echo ERROR: All boot options failed\0" \
+    "bootmenu_1=Boot from NAND Flash=run boot_nand_flash\0" \
+    "bootmenu_2=Boot from eMMC=run boot_mmc1\0" \
+    "bootmenu_3=Boot from NFS=run bootnfs\0" \
+    "default_spl_boot_source=mmc2\0" \
+    "spl_boot_source=mmc2\0"
+#endif /* CONFIG_HAILO15_EMMC_8BIT && CONFIG_HAILO_NAND_BOOT */
 
 #include "hailo1x_common.h"
 
